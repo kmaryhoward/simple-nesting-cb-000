7d28a905bde31123181cec1a578512887c5045c8
Kathleen Howard <kmaryhoward@gmail.com>
2019-02-24 23:33:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
 		 programmer_hash.merge!(:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]})
 end
 
@@ -106,7 +106,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
-
+     programmer_hash
 
 end
 
